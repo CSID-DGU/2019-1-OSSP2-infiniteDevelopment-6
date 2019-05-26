79c81333ba6fa291b7fd284e0ecb3811e05c0822
manist <manist67@naver.com>
2019-05-27 03:36:47 +0900

    Change navigation menus when logged in

--- plass-ide-frontend/src/app/app.component.ts
+++ plass-ide-frontend/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import {
     Component,
+    OnInit
 } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit{
+    isLoggedIn: boolean = false;
+
+    constructor(
+        private dataService: DataService
+    ) {};
+
+    public ngOnInit() {
+        this.dataService.verify().subscribe((value) => {
+            this.isLoggedIn = value;
+        });
+    }
+    public signup() {
+        alert("회원가입은 관리자를 통해 문의하세요.");
+    }
+}
