d31489ae25eae911b781ac4de45315121140f2e8
manist <manist67@naver.com>
2019-05-28 05:07:23 +0900

    Remove unused codes

--- plass-ide-backend/src/v1/router.ts
+++ plass-ide-backend/src/v1/router.ts
@@ -1,6 +1,6 @@
 import * as express from "express";
 import {authenticate} from "./auth";
-import {authEndPoint, fileEndPoint, problemEndPoint, runEndPoint} from "./endPoints";
+import {authEndPoint, problemEndPoint, runEndPoint} from "./endPoints";
 import { ProjectsEndPoint } from "./endPoints/projects";
 
 const router = express.Router();
@@ -10,22 +10,6 @@ router.get("/signout", authEndPoint.signout);
 router.get("/verify", authenticate, authEndPoint.verify);
 
 router.post("/signin", authEndPoint.signin);
-
-router.get("/files", authenticate, fileEndPoint.getFileNames);
-
-router.get("/files/*", authenticate, fileEndPoint.getFile);
-
-router.post("/files", authenticate, fileEndPoint.postFile);
-
-router.put("/files/*", authenticate, fileEndPoint.putFile);
-
-router.delete("/files/*", authenticate, fileEndPoint.deleteFile);
-
-router.post("/rename", authenticate, fileEndPoint.renameFile);
-
-router.post("/run", authenticate, runEndPoint.run);
-
-router.get("/run/result/:hash", authenticate, runEndPoint.result);
 
 router.get("/problems", problemEndPoint.getProblems);
 
@@ -39,7 +23,6 @@ router.post("/projects/:id", authenticate, ProjectsEndPoint.postProject);
 router.put("/projects/:id", authenticate, ProjectsEndPoint.putProject);
 router.delete("/projects/:id", authenticate, ProjectsEndPoint.deleteProject);
 
-
 /**
  * use * for params with slash 
  * index 0 is a file name
@@ -47,5 +30,13 @@ router.delete("/projects/:id", authenticate, ProjectsEndPoint.deleteProject);
 router.get("/projects/:id/*", authenticate, ProjectsEndPoint.getProjectFile);
 router.put("/projects/:id/*", authenticate, ProjectsEndPoint.putProjectFile);
 router.delete("/projects/:id/*", authenticate, ProjectsEndPoint.deleteProjectFile);
+
+/**
+ * run
+ */
+
+router.post("/run", authenticate, runEndPoint.run);
+
+router.get("/run/result/:hash", authenticate, runEndPoint.result);
 
 export {router};
